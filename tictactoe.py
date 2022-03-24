57ba0ab297f3a8bda099f5742542c2aa23c01217
JAngelGL <A01275108@tec.mx>
2022-03-24 16:54:00 -0600

    If applied, this commit will add some comments to the code

--- tictactoe.py
+++ tictactoe.py
@@ -7,13 +7,13 @@ Exercises
 3. How would you detect when someone has won?
 4. How could you create a computer player?
 """
-
+# Importe de librerias turtle y freegames
 from turtle import up, goto, down, circle, update, setup, hideturtle, tracer,\
                    onscreenclick, done
 
 from freegames import line
 
-
+# Añade 4 lineas para formar el campo de juego
 def grid():
     """Draw tic-tac-toe grid."""
     line(-67, 200, -67, -200)
@@ -21,13 +21,13 @@ def grid():
     line(-200, -67, 200, -67)
     line(-200, 67, 200, 67)
 
-
+# Añade dos lineas para formar la X de uno de los jugadores
 def drawx(x, y):
     """Draw X player."""
     line(x, y, x + 133, y + 133)
     line(x, y + 133, x + 133, y)
 
-
+# Añade un circulo de uno de los jugadores
 def drawo(x, y):
     """Draw O player."""
     up()
@@ -35,7 +35,7 @@ def drawo(x, y):
     down()
     circle(62)
 
-
+# Valor redondeado a la cuadrícula con tamaño cuadrado 133
 def floor(value):
     """Round value down to grid with square size 133."""
     return ((value + 200) // 133) * 133 - 200
@@ -44,7 +44,7 @@ def floor(value):
 state = {'player': 0}
 players = [drawx, drawo]
 
-
+# Draw X or O in tapped square
 def tap(x, y):
     """Draw X or O in tapped square."""
     x = floor(x)
